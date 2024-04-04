705f60ac5cd4cea7c3d71ad23dc77d66405f5f0d
zhoushch <zhoushch@shanghaitech.edu.cn>
2024-04-04 14:25:11 +0800

    upload data image

--- homework/homework3/coding/Dataloader.py
+++ homework/homework3/coding/Dataloader.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import seaborn
+import matplotlib.pyplot as plt
 
 class DataLoader:
     def __init__(self, path):
@@ -7,14 +8,11 @@ class DataLoader:
         self.data = self.load_data()
 
         self.process_data()
-        
-        self.data_shuffled = self.data.sample(frac=1, random_state=0).reset_index(drop=True)
-        self.training_data, self.valid_data, self.testing_data = self.split_data()
-   
-        self.training_data = self.training_data.to_numpy()
-        self.valid_data = self.valid_data.to_numpy()
-        self.testing_data = self.testing_data.to_numpy()
 
+        # shuffle the data with fixed random state
+        self.data_shuffled = self.data.sample(frac=1, random_state=0).reset_index(drop=True)
+        self.training_data, self.training_label, self.testing_data, self.testing_label = self.split_data()
+        
     def load_data(self):
         with open(self.path) as f:
             data = pd.read_csv(f)
@@ -22,21 +20,47 @@ class DataLoader:
 
     def process_data(self):
         self.data['Gender'] = self.data['Gender'].map({'Male' : 1, 'Female' : 0})
+
         if len(self.data['User ID'].unique()) == len(self.data):
             print('All elements in the \'User ID\' column is unique')
             self.data = self.data.drop(columns=['User ID'])
-    
+        
+        # seperate 'Age' into 6 groups, and use one-hot encoding
+        # seperate as: 0-20, 20-26, 26-30, 30-40, 40-50, others
+        self.data['Age'].hist(bins=20)
+        plt.xlabel('Age'), plt.ylabel('Number'), plt.title('distribution of Age')
+        plt.show()
+
+        self.data['Age'] = pd.cut(self.data['Age'], bins=[0, 20, 26, 30, 40, 50, 100], labels=[0, 1, 2, 3, 4, 5])
+        seaborn.countplot(x='Purchased', hue='Age', data=self.data)
+        plt.title('sepreate Age into 6 groups')
+        plt.show()
+
+        self.data = pd.get_dummies(self.data, columns=['Age'], dtype=int)
+
+        # seperate 'EstimatedSalary' into 8 groups, and use one-hot encoding
+        # seperate as: 0-19500, 19500-40000, 40000-60000, 60000-80000, 80000-100000, 100000-130000, 130000-145000, others
+        self.data['EstimatedSalary'].hist(bins=20)
+        plt.xlabel('Estimated Salary'), plt.ylabel('Number'), plt.title('distribution of Estimated Salary')
+        plt.show()
+
+        self.data['EstimatedSalary'] = pd.cut(self.data['EstimatedSalary'], bins=[0, 19500, 40000, 60000, 80000, 100000, 130000, 145000, 200000], labels=[0, 1, 2, 3, 4, 5, 6, 7])
+        seaborn.countplot(x='Purchased', hue='EstimatedSalary', data=self.data)
+        plt.title('sepreate Estimated Salary into 8 groups')
+        plt.show()
+
+        self.data = pd.get_dummies(self.data, columns=['EstimatedSalary'], dtype=int)
+
     def split_data(self):
         training_rate = 0.7
-        valid_rate = 0.15
-        testing_rate = 0.15
-
         training_num = int(training_rate * len(self.data_shuffled))
-        valid_num = int(valid_rate * len(self.data_shuffled))
-        testing_num = len(self.data_shuffled) - training_num - valid_num
 
         training_data = self.data_shuffled[:training_num]
-        valid_data = self.data_shuffled[training_num:training_num+valid_num]
-        testing_data = self.data_shuffled[training_num+valid_num:]
+        training_label = training_data['Purchased']
+        training_data = training_data.drop(columns=['Purchased'])
 
-        return training_data, valid_data, testing_data
+        testing_data = self.data_shuffled[training_num:]
+        testing_label = testing_data['Purchased']
+        testing_data = testing_data.drop(columns=['Purchased'])
+
+        return training_data.to_numpy(), training_label.to_numpy().reshape(-1, 1), testing_data.to_numpy(), testing_label.to_numpy().reshape(-1, 1)
--- homework/homework3/coding/Trainer.py
+++ homework/homework3/coding/Trainer.py
@@ -3,39 +3,46 @@ import numpy as np
 import tqdm
 
 class Trainer:
-    def __init__(self, data):
+    from Dataloader import DataLoader
+    def __init__(self, data: DataLoader):
         self.training_data = data.training_data
-        self.valid_data = data.valid_data
-        self.testing_data = data.testing_data
+        self.training_labels = data.training_label
 
-        self.epoch = 1000
+        self.testing_data = data.testing_data
+        self.testing_labels = data.testing_label
+
+        self.epoch = 1000000
         self.eta = 0.1
 
         self.loss = []
         self.lr = []
         self.w_norm = []
 
+        self.dimension = self.training_data.shape[1]
+
     def get_loss(self, x, y, w):
         # logistic loss
-        # X: (N, D), y: (N,), w: (D,) -> scalar
+        # X: (N, D), y: (N, 1), w: (D, 1) -> scalar
         return np.mean(np.log(1 + np.exp(-y * np.dot(x, w))))
 
     def get_gradient(self, x, y, w):
         # logistic gradient
-        # X: (N, D), y: (N,), w: (D,) -> (D,)
-        return -np.mean(y[:, np.newaxis] * x / (1 + np.exp(y * np.dot(x, w))), axis=0)
+        # X: (N, D), y: (N, 1), w: (D, 1) -> (D, 1)
+        return -np.mean(y * x / (1 + np.exp(y * np.dot(x, w))), axis=0).reshape(-1, 1)
     
     def train(self):
-        w = np.zeros
-        
-        # eta_t = eta * |grad_t|
-
+        w = np.zeros(self.dimension).reshape(-1, 1)
         for _ in tqdm.tqdm(range(self.epoch)):
-            gradient_t = self.get_gradient()
+            gradient_t = self.get_gradient(self.training_data, self.training_labels, w)
+            gradient_t_norm = np.linalg.norm(gradient_t)
+            
+            # eta_t = eta * |grad_t|
+            eta_t = self.eta * gradient_t_norm
+            w = w - eta_t * gradient_t
 
-        pass
-
-
+            self.loss.append(self.get_loss(self.training_data, self.training_labels, w))
+            self.lr.append(eta_t)
+            self.w_norm.append(gradient_t_norm)
 
     def print_info(self):
         plt.figure(1)
